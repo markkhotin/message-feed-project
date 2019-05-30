b89d55ce45bee2d6ad63a583bc6c71c498368956
mark500tech <mark@500tech.com>
2019-05-31 00:05:11 +0300

    - ComposeEnhancers fix

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 
 import rootReducer from "reducers/root.reducer";
 import apiMiddleware from "middlewares/api.middleware";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const isDev = process.env.NODE_ENV !== 'production';
+
+const composeEnhancers =
+  (isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const middlewares = [apiMiddleware];
 
